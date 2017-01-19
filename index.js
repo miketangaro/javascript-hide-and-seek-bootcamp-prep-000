5109ab8fe7248725a43b48ce2003ce408f44ae85
Mike Tangaro <mtangaro@gmail.com>
2017-01-19 19:20:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-function getFirstSelector(selector){{
-  document.querySelector('element')
-  return selector
+function getFirstSelector(selector) {
+  var element = document.querySelector(selector)
+  return element
+}
+
+function nestedTarget() {
+  var element = document.querySelector('#nested .target')
+  return element
+}
+
+function increaseRankBy(n) {
+  var element = document.querySelectorAll(".ranked-list")
+
+  for (let i = 0, l = element.length; i < l; i++) {
+      element[i].innerHTML = (i + n).toString()
+  }
+  return element
+}
+
+function deepestChild(){
+  var element = document.getElementById('grand-node').querySelector('div')
+
+  for (let i = 0, l = element.length; i < l; i++) {
+    element[i] === l
+  }
+  return element
 }
